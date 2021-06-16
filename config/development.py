bfd320a2ffeb141f21b072c25bdfae053cdbb51b
lyw1217 <mvl100d@gmail.com>
2021-06-16 17:06:23 +0900

    fixed untracked files

--- config/development.py
+++
@@ -1,8 +0,0 @@
-from config.default import *
-import os
-
-BASE_DIR = os.path.dirname(__file__)
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'ojeommu.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
--- config/production.py
+++
@@ -1,5 +0,0 @@
-from config.default import *
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'ojeommu.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b'b\xdf\n\xbe\x14X\xaal\x96\x1dbd\x92\xe8\x96\x89'
