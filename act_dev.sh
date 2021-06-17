1277d6b14c7fa672b3602e3b2d98cca89764907e
lyw1217 <mvl100d@gmail.com>
2021-06-17 17:51:45 +0900

    스크립트 수정 및 gitignore update

--- act_dev.sh
+++ act_dev.sh
@@ -3,7 +3,4 @@ cd /home/ec2-user/app/OJeomMu
 export APP_CONFIG_FILE=/home/ec2-user/app/OJeomMu/config/development.py
 export FLASK_ENV=development
 export FLASK_APP=app
-source ~/app/OJeomMu/venv/bin/activate
-echo "FLASK RUN [DEV]"
-sleep 1
-flask run
+source /home/ec2-user/app/OJeomMu/venv/bin/activate
