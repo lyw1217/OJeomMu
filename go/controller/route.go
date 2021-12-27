06a58db3b0d66b8cb0c38053d0c229dc8f53f579
lyw1217 <mvl100d@gmail.com>
2021-12-27 22:26:25 +0900

    simple side bar 테스트

--- go/controller/route.go
+++ go/controller/route.go
@@ -27,12 +27,26 @@ func homePage(c *gin.Context) {
 	)
 }
 
+// HomePage : GET, "/"
+// https://startbootstrap.com/template/simple-sidebar
+func homePage2(c *gin.Context) {
+	c.HTML(
+		http.StatusOK,
+		"views/index2.html",
+		gin.H{
+			"key": config.Keys.Kakao.JS,
+		},
+	)
+}
+
 func InitRoutes(r *gin.Engine) {
 
 	r.NoRoute(notFoundPage)
 
 	r.GET("/", homePage)
 	r.GET("/index.html", homePage)
+
+	r.GET("/index2.html", homePage2)
 
 	/* Redirect, for scraping-news-go */
 	r.GET("/maekyung", redirectMaeKyung)
