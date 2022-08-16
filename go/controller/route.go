d98f4569e02af82e16e423d3a3524b1fcbe75483
lyw1217 <mvl100d@gmail.com>
2022-08-16 16:44:59 +0900

    오점무 api화

--- go/controller/route.go
+++ go/controller/route.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"ojeommu/config"
@@ -59,41 +60,151 @@ func TestPage(c *gin.Context) {
 }
 
 func SearchHandler(c *gin.Context) {
-
 	var jsonData SearchCond_t
+	var qry_result KeywordDocuments_t
 	if c.BindJSON(&jsonData) == nil {
+		qry := c.Query("query")
+		if len(qry) > 0 {
+
+			var p = KeywordParam_t{
+				Query: qry,
+				Page:  1,
+				Size:  15,
+				Sort:  "accuracy",
+			}
+			fmt.Println(p)
+			tmp, err := GetSearchKeyword(p, 500)
+			if err != nil {
+				log.Println("Error, Failed GetSearchKeyword()")
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"status": http.StatusInternalServerError,
+					"reason": "Internal Server Error",
+				})
+				return
+			}
+			jsonData.Category = "FD6"
+			jsonData.Radius = "0.5"
+			if len(tmp) > 0 {
+				jsonData.X = tmp[0].X
+				jsonData.Y = tmp[0].Y
+				qry_result.PlaceName = tmp[0].PlaceName
+			} else {
+				c.JSON(http.StatusNotFound, gin.H{
+					"status": http.StatusNotFound,
+					"reason": "Not Found",
+				})
+				return
+			}
+		}
 		matched_place, total_nums, err := RectSearch(jsonData)
 
 		if err != nil {
 			log.Println("Error, failed RectSearch()")
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status": http.StatusInternalServerError,
+				"reason": "Internal Server Error",
+			})
 			return
 		}
 		log.Println("matched_place =", matched_place)
 		if matched_place == nil {
 			log.Println("Error, failed GetCondPlace()")
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status": http.StatusInternalServerError,
+				"reason": "Internal Server Error",
+			})
 		} else {
 			// 현재 위치와 place간 거리 구하기
 			d := GetDistance(jsonData.X, jsonData.Y, matched_place.X, matched_place.Y)
 
 			// 데이터 전송
 			c.JSON(200, gin.H{
-				"ID":           matched_place.Id,
-				"NAME":         matched_place.PlaceName,
-				"CAT_NAME":     matched_place.CategoryName,
-				"PHONE":        matched_place.Phone,
-				"ADDRESS":      matched_place.AddressName,
-				"ROAD_ADDRESS": matched_place.RoadAddressName,
-				"X":            matched_place.X,
-				"Y":            matched_place.Y,
-				"URL":          matched_place.PlaceUrl,
-				"DISTANCE":     d,
-				"TOTAL_NUMS":   total_nums,
+				"ID":             matched_place.Id,
+				"NAME":           matched_place.PlaceName,
+				"CAT_NAME":       matched_place.CategoryName,
+				"PHONE":          matched_place.Phone,
+				"ADDRESS":        matched_place.AddressName,
+				"ROAD_ADDRESS":   matched_place.RoadAddressName,
+				"X":              matched_place.X,
+				"Y":              matched_place.Y,
+				"URL":            matched_place.PlaceUrl,
+				"DISTANCE":       d,
+				"TOTAL_NUMS":     total_nums,
+				"QRY_PLACE_NAME": qry_result.PlaceName,
 			})
 		}
 
 	} else {
 		// handle error
 		log.Println("Error, failed BindJSON()")
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": http.StatusInternalServerError,
+			"reason": "Internal Server Error",
+		})
+		return
+	}
+}
+
+func SearchBotHandler(c *gin.Context) {
+	var jsonData SearchCond_t
+	var qry_result KeywordDocuments_t
+
+	qry := c.Query("query")
+	if len(qry) > 0 {
+
+		var p = KeywordParam_t{
+			Query: qry,
+			Page:  1,
+			Size:  15,
+			Sort:  "accuracy",
+		}
+		fmt.Println(p)
+		tmp, err := GetSearchKeyword(p, 500)
+		if err != nil {
+			log.Println("Error, Failed GetSearchKeyword()")
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status": http.StatusInternalServerError,
+				"reason": "Internal Server Error",
+			})
+			return
+		}
+		jsonData.Category = "anything"
+		jsonData.Radius = "0.5"
+		if len(tmp) > 0 {
+			jsonData.X = tmp[0].X
+			jsonData.Y = tmp[0].Y
+			qry_result.PlaceName = tmp[0].PlaceName
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"status": http.StatusNotFound,
+				"reason": "Not Found",
+			})
+			return
+		}
+	}
+	matched_place, _, err := RectSearch(jsonData)
+
+	if err != nil {
+		log.Println("Error, failed RectSearch()")
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": http.StatusInternalServerError,
+			"reason": "Internal Server Error",
+		})
+		return
+	}
+	log.Println("matched_place =", matched_place)
+	if matched_place == nil {
+		log.Println("Error, failed GetCondPlace()")
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": http.StatusInternalServerError,
+			"reason": "Internal Server Error",
+		})
+		return
+	} else {
+		// 현재 위치와 place간 거리 구하기
+		d := GetDistance(jsonData.X, jsonData.Y, matched_place.X, matched_place.Y)
+
+		c.String(http.StatusOK, "오늘 점심은 '%s' 어떠세요?\n------------------------------------------\n %s (으)로부터 거리는 %dm\n URL : %s", matched_place.PlaceName, qry_result.PlaceName, d, matched_place.PlaceUrl)
 		return
 	}
 }
@@ -108,6 +219,7 @@ func InitRoutes(r *gin.Engine) {
 	r.GET("/test.html", TestPage)
 
 	r.POST("/sendToGo", SearchHandler)
+	r.POST("/ojeommu", SearchBotHandler)
 
 	/* Redirect, for scraping-news-go */
 	r.GET("/maekyung", RedirectMaeKyung)
