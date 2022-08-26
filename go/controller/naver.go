a872f3a66c398d196eb4ff561ed25eecbaddb17f
lyw1217 <mvl100d@gmail.com>
2022-08-26 14:46:57 +0900

    랜덤 검색 방식 변경

--- go/controller/naver.go
+++ go/controller/naver.go
@@ -23,61 +23,61 @@ func GetNaverLocal(p LocalParam_t) (*LocalDocument_t, error) {
 
 	data := []LocalDocument_t{}
 
-	for i := 0; i < len(append_qry); i++ {
-		for j := 0; j < len(sort_qry); j++ {
-			baseUrl, err := url.Parse(NaverSearchLocalUrl)
-			if err != nil {
-				log.Println("Malformed URL: ", err.Error())
-				return nil, err
-			}
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(len(append_qry))
 
-			p.Query = fmt.Sprintf("%s %s", p.Query, append_qry[i])
-			p.Sort = sort_qry[j]
-			vals, _ := query.Values(p)
-			// Add Query Parameters to the URL
-			baseUrl.RawQuery = vals.Encode() // Escape Query Parameters
-
-			req, err := http.NewRequest("GET", baseUrl.String(), nil)
-			if err != nil {
-				log.Println(err)
-				return nil, err
-			}
-			k := config.Keys.Naver
-			req.Header.Add("X-Naver-Client-Id", k.Id)
-			req.Header.Add("X-Naver-Client-Secret", k.Secret)
-
-			client := &http.Client{}
-			rsp, err := client.Do(req)
-			if err != nil {
-				log.Println(err)
-				return nil, err
-			}
-			defer rsp.Body.Close()
-
-			rspBody, err := ioutil.ReadAll(rsp.Body)
-			if err != nil {
-				str := string(rspBody)
-				log.Println(str)
-				return nil, err
-			}
-
-			var tmp LocalDocument_t
-			err = json.Unmarshal(rspBody, &tmp)
-			if err != nil {
-				log.Printf("Error occured during unmarshaling. Error: %s", err.Error())
-				return nil, err
-			}
-
-			if tmp.ErrorCode == "" {
-				data = append(data, tmp)
-			}
-
-			time.Sleep(time.Millisecond * 2)
+	for j := 0; j < len(sort_qry); j++ {
+		baseUrl, err := url.Parse(NaverSearchLocalUrl)
+		if err != nil {
+			log.Println("Malformed URL: ", err.Error())
+			return nil, err
 		}
+
+		p.Query = fmt.Sprintf("%s %s", p.Query, append_qry[n])
+		p.Sort = sort_qry[j]
+		vals, _ := query.Values(p)
+		// Add Query Parameters to the URL
+		baseUrl.RawQuery = vals.Encode() // Escape Query Parameters
+
+		req, err := http.NewRequest("GET", baseUrl.String(), nil)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		k := config.Keys.Naver
+		req.Header.Add("X-Naver-Client-Id", k.Id)
+		req.Header.Add("X-Naver-Client-Secret", k.Secret)
+
+		client := &http.Client{}
+		rsp, err := client.Do(req)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		defer rsp.Body.Close()
+
+		rspBody, err := ioutil.ReadAll(rsp.Body)
+		if err != nil {
+			str := string(rspBody)
+			log.Println(str)
+			return nil, err
+		}
+
+		var tmp LocalDocument_t
+		err = json.Unmarshal(rspBody, &tmp)
+		if err != nil {
+			log.Printf("Error occured during unmarshaling. Error: %s", err.Error())
+			return nil, err
+		}
+
+		if tmp.ErrorCode == "" {
+			data = append(data, tmp)
+		}
+
+		time.Sleep(time.Millisecond * 2)
 	}
 
-	rand.Seed(time.Now().UnixNano())
-	n := rand.Intn(len(data))
+	n = rand.Intn(len(sort_qry))
 
-	return &data[n%len(data)], nil
+	return &data[n], nil
 }
