8fb77f98e46735ffb16578653a75377dd96a1f25
lyw1217 <mvl100d@gmail.com>
2022-11-25 17:00:03 +0900

    dockerfile 통합

--- go/scripts/docker_build_app.sh
+++ go/scripts/docker_build_app.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-BASE_VER=0.2
-APP_VER=0.4
+#BASE_VER=0.2
+#APP_VER=0.4
+#
+#cp -f ../Dockerfile ../Dockerfile-bkup
+#cp -f ../Dockerfile-app ../Dockerfile
+#
+#docker build \
+#    --build-arg ssh_pub_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+MUkTMHfVBLcA0TgRMDmhKH5kjuDX2nX8hVMcuu4yJMNsc+eG7j+n2fCs87pobg+E2QxIrVLeKF5fwmdKpfThNV+xPLSdSQdSWSQbXDvPeMzotO77LAxOwquAR3vePrSfj7sNXsP+hg1fSluYaC32PlFZB7WvoiZ6VNF0hnaapdlt4NQZI5ZZGOnKuU1ytXoa0OC/sIyHgEvnB4Yk3k/PzUJuFoOjvM9B/CLsI60vrrARQGQn9WJSbpKi87IfdtH54iWAvGx24DiRukLl60MyeojyByMpNRX93mHPtxjLmfsep7vjoBTti/2y6oqVkpzpPAZc4T2GEyPnWFnli/5r3ZNNTpKtT/XlYaZLOhiIwC5OzuzlpUrbvaCeWlIgxktrc4ijb3NLFlzXYFp01RJ/rmoqHKsmbT55pGWQ6pCcZmZoncsoHvOgX3GSGMRJvMJjMUIDr8in1N1IqgqvEBokhxk+u055djuz17Jlojq44UnylbXSRQziymXTNJTmYEk= jenkins@a7131a1767c9" \
+#    --build-arg BUILD_IMAGE=lyw1217/ojeommu-base:${BASE_VER} \
+#    -t lyw1217/ojeommu:${APP_VER} "../"
 
-cp -f ../Dockerfile ../Dockerfile-bkup
-cp -f ../Dockerfile-app ../Dockerfile
+#TAG="0.5"
+TAG="0.6"
 
-docker build \
-    --build-arg ssh_pub_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+MUkTMHfVBLcA0TgRMDmhKH5kjuDX2nX8hVMcuu4yJMNsc+eG7j+n2fCs87pobg+E2QxIrVLeKF5fwmdKpfThNV+xPLSdSQdSWSQbXDvPeMzotO77LAxOwquAR3vePrSfj7sNXsP+hg1fSluYaC32PlFZB7WvoiZ6VNF0hnaapdlt4NQZI5ZZGOnKuU1ytXoa0OC/sIyHgEvnB4Yk3k/PzUJuFoOjvM9B/CLsI60vrrARQGQn9WJSbpKi87IfdtH54iWAvGx24DiRukLl60MyeojyByMpNRX93mHPtxjLmfsep7vjoBTti/2y6oqVkpzpPAZc4T2GEyPnWFnli/5r3ZNNTpKtT/XlYaZLOhiIwC5OzuzlpUrbvaCeWlIgxktrc4ijb3NLFlzXYFp01RJ/rmoqHKsmbT55pGWQ6pCcZmZoncsoHvOgX3GSGMRJvMJjMUIDr8in1N1IqgqvEBokhxk+u055djuz17Jlojq44UnylbXSRQziymXTNJTmYEk= jenkins@a7131a1767c9" \
-    --build-arg BUILD_IMAGE=lyw1217/ojeommu-base:${BASE_VER} \
-    -t lyw1217/ojeommu:${APP_VER} "../"
+#docker build --tag lyw1217/ojeommu:${TAG} "../"
+#docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag lyw1217/ojeommu:${TAG} "../"
+docker buildx build --push --platform linux/amd64 --tag lyw1217/ojeommu:${TAG} "../"
--- go/scripts/docker_build_base.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASE_VER=0.2
-
-cp -f ../Dockerfile ../Dockerfile-bkup
-cp -f ../Dockerfile-base ../Dockerfile
-
-docker build -t lyw1217/ojeommu-base:${BASE_VER} "../"
